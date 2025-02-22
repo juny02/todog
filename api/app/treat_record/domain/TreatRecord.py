59c8725bea853dcde5b746028b3a1e82ecc9915c
juny02 <qapl12321@gmail.com>
2025-02-22 20:45:57 +0900

    feat(api/treat-record): add treat_record domain

---
+++ api/app/treat_record/domain/TreatRecord.py
@@ -0,0 +1,13 @@
+from pydantic import AwareDatetime, BaseModel, Field
+
+
+class TreatRecord(BaseModel):
+    id: str
+    dog_id: str
+    user_id: str
+    treat_id: str
+    quantity: int
+    given_at: AwareDatetime
+    description: str | None
+
+
